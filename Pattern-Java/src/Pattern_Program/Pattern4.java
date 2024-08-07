0f4a54a1568781c79271e8121a79c7ea65b18b5f
Pratik Sutar <112809787+pratiksutar841@users.noreply.github.com>
2024-08-07 18:14:54 +0530

    Pattern-Java

---
+++ Pattern-Java/src/Pattern_Program/Pattern4.java
@@ -0,0 +1,24 @@
+package Pattern_Program;
+
+public class Pattern4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 int n=5;
+		  for(int i = 0; i < n; i++) 
+		     {
+		      for(int j = 0; j < n; j++) 
+		         {
+		    	  if (i==(n-1)/2||j==0||j==n-1) 
+		    	  {
+					System.out.print("*");
+				  } else 
+				  {
+	              System.out.print(" ");
+				  }
+			     }
+		      System.out.println();
+		     }	
+	}
+
+}
